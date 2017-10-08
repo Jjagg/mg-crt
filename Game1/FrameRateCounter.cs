81bcf73e02f1087a6223b14aa97145478ead45fd
Jesse Gielen <jessegielen@hotmail.com>
2017-10-08 15:41:32 +0200

    Initial commit 220 fps, 4.5ms

---
+++ Game1/FrameRateCounter.cs
@@ -0,0 +1,88 @@
+﻿// Adapted from the following blog post by Shawn Hargreaves.
+// https://blogs.msdn.microsoft.com/shawnhar/2007/06/08/displaying-the-framerate/
+
+using System;
+using Game1.Properties;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game1
+{
+    public class FrameRateCounter : DrawableGameComponent
+    {
+        private readonly ContentManager _content;
+        private SpriteBatch _spriteBatch;
+
+        private int _frameRate;
+        private int _frameCounter;
+        private TimeSpan _elapsedTime = TimeSpan.Zero;
+
+        public Vector2 Position = new Vector2(5, 5);
+        private SpriteFont _font;
+        private int _fontLineSpacing;
+
+        public SpriteFont Font
+        {
+            get { return _font; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(value));
+                _font = value;
+                _fontLineSpacing = Font.LineSpacing;
+            }
+        }
+
+        /// <summary>
+        /// Create a new frame counter for the given game.
+        /// </summary>
+        /// <param name="game"></param>
+        public FrameRateCounter(Game game) : base(game)
+        {
+            var resourceManager = Resources.ResourceManager;
+            _content = new ResourceContentManager(game.Services, resourceManager);
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            Font = _content.Load<SpriteFont>("fpsFont");
+        }
+
+        protected override void UnloadContent()
+        {
+            _content.Unload();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            _elapsedTime += gameTime.ElapsedGameTime;
+
+            if (_elapsedTime > TimeSpan.FromSeconds(1))
+            {
+                _elapsedTime -= TimeSpan.FromSeconds(1);
+                _frameRate = _frameCounter;
+                _frameCounter = 0;
+            }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            _frameCounter++;
+
+            var fps = $"fps: {_frameRate}";
+            var spf = $"spf: {(_frameRate == 0 ? 1 : 1000f/_frameRate).ToString("n4")}";
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.DrawString(Font, fps, Position, Color.Black);
+            _spriteBatch.DrawString(Font, fps, Position - Vector2.One, Color.White);
+
+            _spriteBatch.DrawString(Font, spf, Position + _fontLineSpacing * Vector2.UnitY, Color.Black);
+            _spriteBatch.DrawString(Font, spf, Position - Vector2.One + _fontLineSpacing * Vector2.UnitY, Color.White);
+
+            _spriteBatch.End();
+        }
+    }
+}
---
+++ Game1/Game1.cs
@@ -0,0 +1,101 @@
+﻿using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        private readonly GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private SpriteFont _font;
+        private Texture2D _blank;
+
+        private Texture2D _texture;
+        private Effect _effect;
+        
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.SynchronizeWithVerticalRetrace = false;
+            IsFixedTimeStep = false;
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            Components.Add(new FrameRateCounter(this));
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _texture = Content.Load<Texture2D>("EagleIsland");
+            _font = Content.Load<SpriteFont>("font");
+            _blank = new Texture2D(GraphicsDevice, 1, 1);
+            _blank.SetData(new[] {Color.White.PackedValue});
+
+            _graphics.PreferredBackBufferWidth = _texture.Width;
+            _graphics.PreferredBackBufferHeight = _texture.Height;
+            _graphics.ApplyChanges();
+
+            _effect = Content.Load<Effect>("crt-lottes-mg");
+            _effect.Parameters["hardScan"]?.SetValue(-8.0f);
+            _effect.Parameters["hardPix"]?.SetValue(-3.0f);
+            _effect.Parameters["warpX"]?.SetValue(0.031f);
+            _effect.Parameters["warpY"]?.SetValue(0.041f);
+            _effect.Parameters["maskDark"]?.SetValue(0.5f);
+            _effect.Parameters["maskLight"]?.SetValue(1.5f);
+            _effect.Parameters["scaleInLinearGamma"]?.SetValue(1.0f);
+            _effect.Parameters["shadowMask"]?.SetValue(3.0f);
+            _effect.Parameters["brightboost"]?.SetValue(1.0f);
+            _effect.Parameters["hardBloomScan"]?.SetValue(-1.5f);
+            _effect.Parameters["hardBloomPix"]?.SetValue(-2.0f);
+            _effect.Parameters["bloomAmount"]?.SetValue(0.15f);
+            _effect.Parameters["shape"]?.SetValue(2.0f);
+
+            _effect.Parameters["textureSize"].SetValue(new Vector2(_texture.Width, _texture.Height));
+            _effect.Parameters["videoSize"].SetValue(new Vector2(_texture.Width, _texture.Height));
+            _effect.Parameters["outputSize"].SetValue(new Vector2(_graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight));
+
+            var vp = GraphicsDevice.Viewport;
+            var output = new RenderTarget2D(GraphicsDevice, vp.Width, vp.Height);
+            GraphicsDevice.SetRenderTarget(output);
+
+            _spriteBatch.Begin(effect: _effect);
+            _spriteBatch.Draw(_texture, Vector2.Zero, Color.White);
+            _spriteBatch.End();
+
+            GraphicsDevice.SetRenderTarget(null);
+            using (var fs = File.OpenWrite("../../../../../img.png"))
+                output.SaveAsPng(fs, output.Width, output.Height);
+            output.Dispose();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin(effect: _effect);
+            _spriteBatch.Draw(_texture, Vector2.Zero, Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
